84b2bcd98e4ae21ec9f2184ae0c5172853788660
Keith Stanigar <ktstanigar@hotmail.com>
2020-07-27 14:30:39 +0200

    Changed timer to 11 seconds

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,10 @@
 // Income Ticker Display (displaying every 10seconds
-var questionTicker = 12;
+var questionTicker = 11;
 
 window.setInterval(function () {
     if (questionTicker > 0)
         questionTicker--;
     document.getElementById("questionTicker").innerHTML = "Next Question In : " + questionTicker;
     if (questionTicker <= 0)
-        questionTicker = 12;
+        questionTicker = 11;
 }, 1000);
