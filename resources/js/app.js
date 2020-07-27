befbff5c1b15c3160298dac4104ec8f442b2e981
Keith Stanigar <ktstanigar@hotmail.com>
2020-07-27 10:39:55 +0200

    Changed carousel time to 12 seconds

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,10 @@
 // Income Ticker Display (displaying every 10seconds
-var questionTicker = 10;
+var questionTicker = 12;
 
 window.setInterval(function () {
     if (questionTicker > 0)
         questionTicker--;
     document.getElementById("questionTicker").innerHTML = "Next Question In : " + questionTicker;
     if (questionTicker <= 0)
-        questionTicker = 10;
+        questionTicker = 12;
 }, 1000);
