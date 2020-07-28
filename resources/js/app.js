f75da22da02e43a8457275b5345a3a66267fa185
Keith Stanigar <ktstanigar@hotmail.com>
2020-07-28 10:11:27 +0200

    Added countdown to top

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 // Income Ticker Display (displaying every 10seconds
 var questionTicker = 11;
+var questionTicker2 = 11;
 
 window.setInterval(function () {
     if (questionTicker > 0)
@@ -7,4 +8,12 @@ window.setInterval(function () {
     document.getElementById("questionTicker").innerHTML = "Next Question In : " + questionTicker;
     if (questionTicker <= 0)
         questionTicker = 11;
+}, 1000);
+
+window.setInterval(function () {
+    if (questionTicker2 > 0)
+        questionTicker2--;
+    document.getElementById("questionTicker2").innerHTML = "Next Question In : " + questionTicker2;
+    if (questionTicker2 <= 0)
+        questionTicker2 = 11;
 }, 1000);
