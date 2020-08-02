fc4d5ad973334b1be9cad3aeb87aaf2000cf260e
Keith Stanigar <ktstanigar@hotmail.com>
2020-08-02 19:21:10 +0200

    Changed carousel layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,13 @@
 // Income Ticker Display (displaying every 10seconds
-var questionTicker = 11;
-var questionTicker2 = 11;
+var questionTicker = 7;
+var questionTicker2 = 7;
 
 window.setInterval(function () {
     if (questionTicker > 0)
         questionTicker--;
     document.getElementById("questionTicker").innerHTML = "Next Question In : " + questionTicker;
     if (questionTicker <= 0)
-        questionTicker = 11;
+        questionTicker = 7;
 }, 1000);
 
 window.setInterval(function () {
@@ -15,5 +15,5 @@ window.setInterval(function () {
         questionTicker2--;
     document.getElementById("questionTicker2").innerHTML = "Next Question In : " + questionTicker2;
     if (questionTicker2 <= 0)
-        questionTicker2 = 11;
+        questionTicker2 = 7;
 }, 1000);
