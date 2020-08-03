054af5379dd51438c880a00b150c5e295cb4f54d
Keith Stanigar <ktstanigar@hotmail.com>
2020-08-03 11:45:43 +0200

    Changed countdown ticker to 8 seconds

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,13 @@
-// Income Ticker Display (displaying every 10seconds
-var questionTicker = 7;
-var questionTicker2 = 7;
+// Income Ticker Display (displaying every 8seconds
+var questionTicker = 8;
+var questionTicker2 = 8;
 
 window.setInterval(function () {
     if (questionTicker > 0)
         questionTicker--;
     document.getElementById("questionTicker").innerHTML = "Next Question In : " + questionTicker;
     if (questionTicker <= 0)
-        questionTicker = 7;
+        questionTicker = 8;
 }, 1000);
 
 window.setInterval(function () {
@@ -15,5 +15,5 @@ window.setInterval(function () {
         questionTicker2--;
     document.getElementById("questionTicker2").innerHTML = "Next Question In : " + questionTicker2;
     if (questionTicker2 <= 0)
-        questionTicker2 = 7;
+        questionTicker2 = 8;
 }, 1000);
