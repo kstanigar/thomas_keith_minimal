4fca38776d8004ccfa40ae93397daab1797f5af5
Keith Stanigar <ktstanigar@hotmail.com>
2020-08-06 14:48:54 +0200

    Put ticker in slider and simplified margin-top

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,9 @@
 // Income Ticker Display (displaying every 8seconds
 var questionTicker = 8;
+var questionTicker1 = 8;
 var questionTicker2 = 8;
+var questionTicker3 = 8;
+var questionTicker4 = 8;
 
 window.setInterval(function () {
     if (questionTicker > 0)
@@ -11,9 +14,33 @@ window.setInterval(function () {
 }, 1000);
 
 window.setInterval(function () {
+    if (questionTicker1 > 0)
+        questionTicker1--;
+    document.getElementById("questionTicker1").innerHTML = "Next Question In : " + questionTicker1;
+    if (questionTicker1 <= 0)
+        questionTicker1 = 8;
+}, 1000);
+
+window.setInterval(function () {
     if (questionTicker2 > 0)
         questionTicker2--;
     document.getElementById("questionTicker2").innerHTML = "Next Question In : " + questionTicker2;
     if (questionTicker2 <= 0)
         questionTicker2 = 8;
+}, 1000);
+
+window.setInterval(function () {
+    if (questionTicker3 > 0)
+        questionTicker3--;
+    document.getElementById("questionTicker3").innerHTML = "Next Question In : " + questionTicker3;
+    if (questionTicker3 <= 0)
+        questionTicker3 = 8;
+}, 1000);
+
+window.setInterval(function () {
+    if (questionTicker4 > 0)
+        questionTicker4--;
+    document.getElementById("questionTicker4").innerHTML = "Next Question In : " + questionTicker4;
+    if (questionTicker4 <= 0)
+        questionTicker4 = 8;
 }, 1000);
