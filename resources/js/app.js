de5e12c10da9643620a1053ecaa5fd3a72c9e639
Keith Stanigar <ktstanigar@hotmail.com>
2020-08-22 01:26:14 +0200

    Changed questions and changed timer to 10sec

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,32 +1,32 @@
 // Income Ticker Display (displaying every 8seconds
-var questionTicker = 8;
-var questionTicker1 = 8;
-var questionTicker2 = 8;
-var questionTicker3 = 8;
-var questionTicker4 = 8;
+var questionTicker = 10;
+var questionTicker1 = 10;
+var questionTicker2 = 10;
+var questionTicker3 = 10;
+var questionTicker4 = 10;
 
 window.setInterval(function () {
     if (questionTicker > 0)
         questionTicker--;
-    document.getElementById("questionTicker").innerHTML = "Question coming in : " + questionTicker;
+    document.getElementById("questionTicker").innerHTML = "Wait for your first Question : " + questionTicker;
     if (questionTicker <= 0)
-        questionTicker = 8;
+        questionTicker = 10;
 }, 1000);
 
 window.setInterval(function () {
     if (questionTicker1 > 0)
         questionTicker1--;
-    document.getElementById("questionTicker1").innerHTML = "Next Question In : " + questionTicker1;
+    document.getElementById("questionTicker1").innerHTML = "Wait for the Next Question In : " + questionTicker1;
     if (questionTicker1 <= 0)
-        questionTicker1 = 8;
+        questionTicker1 = 10;
 }, 1000);
 
 window.setInterval(function () {
     if (questionTicker2 > 0)
         questionTicker2--;
-    document.getElementById("questionTicker2").innerHTML = "Next Question In : " + questionTicker2;
+    document.getElementById("questionTicker2").innerHTML = "Wait for the Next Question In : " + questionTicker2;
     if (questionTicker2 <= 0)
-        questionTicker2 = 8;
+        questionTicker2 = 10;
 }, 1000);
 
 window.setInterval(function () {
@@ -34,7 +34,7 @@ window.setInterval(function () {
         questionTicker3--;
     document.getElementById("questionTicker3").innerHTML = "Next Question In : " + questionTicker3;
     if (questionTicker3 <= 0)
-        questionTicker3 = 8;
+        questionTicker3 = 10;
 }, 1000);
 
 window.setInterval(function () {
@@ -42,5 +42,5 @@ window.setInterval(function () {
         questionTicker4--;
     document.getElementById("questionTicker4").innerHTML = "Re-introduction In : " + questionTicker4;
     if (questionTicker4 <= 0)
-        questionTicker4 = 8;
+        questionTicker4 = 10;
 }, 1000);
