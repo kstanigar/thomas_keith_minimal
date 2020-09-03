9431714193845391f22915f3db8812e1449bf5e0
Keith Stanigar <ktstanigar@hotmail.com>
2020-09-03 22:22:41 +0200

    Added animation to arrow and changed hints

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,7 +8,7 @@ var questionTicker4 = 10;
 window.setInterval(function () {
     if (questionTicker > 0)
         questionTicker--;
-    document.getElementById("questionTicker").innerHTML = "Wait for your first Question : " + questionTicker;
+    document.getElementById("questionTicker").innerHTML = "WAIT for your first Question : " + questionTicker;
     if (questionTicker <= 0)
         questionTicker = 10;
 }, 1000);
@@ -16,7 +16,7 @@ window.setInterval(function () {
 window.setInterval(function () {
     if (questionTicker1 > 0)
         questionTicker1--;
-    document.getElementById("questionTicker1").innerHTML = "Wait for the Next Question In : " + questionTicker1;
+    document.getElementById("questionTicker1").innerHTML = "WAIT for the Next Question : " + questionTicker1;
     if (questionTicker1 <= 0)
         questionTicker1 = 10;
 }, 1000);
@@ -24,7 +24,7 @@ window.setInterval(function () {
 window.setInterval(function () {
     if (questionTicker2 > 0)
         questionTicker2--;
-    document.getElementById("questionTicker2").innerHTML = "Wait for the Next Question In : " + questionTicker2;
+    document.getElementById("questionTicker2").innerHTML = "WAIT for the Next Question : " + questionTicker2;
     if (questionTicker2 <= 0)
         questionTicker2 = 10;
 }, 1000);
@@ -32,7 +32,7 @@ window.setInterval(function () {
 window.setInterval(function () {
     if (questionTicker3 > 0)
         questionTicker3--;
-    document.getElementById("questionTicker3").innerHTML = "Wait for the Next Question In : " + questionTicker3;
+    document.getElementById("questionTicker3").innerHTML = "WAIT for the Next Question : " + questionTicker3;
     if (questionTicker3 <= 0)
         questionTicker3 = 10;
 }, 1000);
@@ -40,7 +40,7 @@ window.setInterval(function () {
 window.setInterval(function () {
     if (questionTicker4 > 0)
         questionTicker4--;
-    document.getElementById("questionTicker4").innerHTML = "Re-introduction In : " + questionTicker4;
+    document.getElementById("questionTicker4").innerHTML = "Re-introduction : " + questionTicker4;
     if (questionTicker4 <= 0)
         questionTicker4 = 10;
 }, 1000);
